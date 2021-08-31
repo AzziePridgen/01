243891fcc8cf7199155bb69c1b87f7b3dcd292a9
xycas <89785944+xycas@users.noreply.github.com>
2021-08-31 21:49:50 +0700

    Create sbi.sh

---
+++ sbi.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DEvQGELtsRemCLZpnUbXsndRWYK2qdVkZT.s8i -p c=DGB,mc=VRSC,d=2000
